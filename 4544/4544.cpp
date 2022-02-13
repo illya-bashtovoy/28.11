85ed678b77927ac9fc74bac73b324e24e2778b88
ibash <ibash@IBASH>
2022-02-13 03:59:16 +0200

    Добавьте файлы проекта.

---
+++ 4544/4544.cpp
@@ -0,0 +1,18 @@
+﻿/*
+Баштовий.І.В
+1. Вивести таблицю множення від 1 до числа n
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int n;
+    cout << "enter_n:\n";
+    cin >> n;
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j <= n; j++) {
+            cout << i * j << "\t";
+        }
+    }
+}
+
